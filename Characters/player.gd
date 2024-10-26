cf7b78dcaee4998997476ad7589195a5f77cd2c9
Andrej Dusa <dusa.andrej@scheidt-bachmann.sk>
2024-10-26 11:29:31 +0200

    Fixed error issue with tile map

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-@export var MAX_SPEED = 300
-@export var ACCELERATION = 1500
-@export var FRICTION = 1300
+@export var MAX_SPEED = 200
+@export var ACCELERATION = 1000
+@export var FRICTION = 950
 
 @onready var axis = Vector2.ZERO
 
