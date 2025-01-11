fd1a91bb7370a585f719ec180980782228d86fb3
OnePCtoRuleThemAll <101508838+OnePCtoRuleThemAll@users.noreply.github.com>
2025-01-11 20:43:52 +0100

    sound

--- GUI/Scripts/main_menu.gd
+++ GUI/Scripts/main_menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 @onready var intro_scene = preload("res://Game/Scenes/intro.tscn") as PackedScene
+@onready var anim = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus()
+	anim.play("pc")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
