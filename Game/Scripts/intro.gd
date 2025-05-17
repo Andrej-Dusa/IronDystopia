b9495db19d140a4bf14d7be4c0358240fee2df2b
Andrej Dusa <dusa.andrej@scheidt-bachmann.sk>
2025-05-17 13:17:56 +0200

    Added few setting plus intro scene skip feature

--- Game/Scripts/intro.gd
+++ Game/Scripts/intro.gd
@@ -3,10 +3,17 @@ extends Node2D
 @onready var animation_player = $AnimatedSprite2D
 @onready var dungeon_scene = preload("res://Game/Scenes/dungeon.tscn") as PackedScene
 
+var cutscene_skipped = false
+
 func _ready():
 	var timer = $Timer
 	timer.start(2)
 	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept") and not cutscene_skipped:
+		cutscene_skipped = true
+		load_dungeon()
 
 func load_dungeon():
 	if dungeon_scene:
@@ -18,7 +25,9 @@ func load_dungeon():
 	#get_tree().change_scene_to_packed(dungeon_scene)
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	load_dungeon()
+	if not cutscene_skipped:
+		cutscene_skipped = true
+		load_dungeon()
 
 
 func _on_ready() -> void:
